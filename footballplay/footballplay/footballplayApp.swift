15ba2a491628dbc490ef8b37a1f50bc77808962d
fredericli <fredericli@cht.com.tw>
2025-10-25 19:51:36 +0800

    專案初始化完成

---
+++ footballplay/footballplay/footballplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballplayApp.swift
+//  footballplay
+//
+//  Created by fredericli on 2025/10/25.
+//
+
+import SwiftUI
+
+@main
+struct footballplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ footballplay/footballplayTests/footballplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  footballplayTests.swift
+//  footballplayTests
+//
+//  Created by fredericli on 2025/10/25.
+//
+
+import Testing
+@testable import footballplay
+
+struct footballplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
